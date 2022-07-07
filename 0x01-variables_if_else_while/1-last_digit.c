9ab96593bde562aa6820a1e1a8b3075050bde7e1
chimi boris <57298424+chimiboris@users.noreply.github.com>
2022-07-07 13:21:45 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,9 +2,10 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main - Determine if a random number is positive, negative or zero.
+ * main- Entry Point
  *
- * Return: 0 on success
+ * Return: Always 0 (success)
+ *
  */
 int main(void)
 {
