e5b2d737f069201690a0080091f5ee1baea11620
chimi boris <57298424+chimiboris@users.noreply.github.com>
2022-07-07 14:56:44 +0100

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #include <stdio.h>
 #include "main.h"
 
@@ -25,31 +24,3 @@ int main(void)
 		return (0);
 
 }
-=======
-#include <stdio.h>
-#include "main.h"
-
-/**
- * main - Entry point
- * Description: prints 'Holberton \n'
- * Return: Always 0 (success)
- */
-
-int main(void)
-
-{
-
-char str[] = "_putchar\n";
-int i = 0;
-
-while (str[i] != '\0')
-{
-
-char c = str[i];
-_putchar(c);
-i++;
-}
-	return (0);
-
-}
->>>>>>> fd426e9ddecb409633a43ce953c45f01c13d4984
