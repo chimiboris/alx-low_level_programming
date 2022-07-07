94234228ee18ff2bf2a342d8e15177ee83a4b8f3
chimi boris <57298424+chimiboris@users.noreply.github.com>
2022-07-07 17:29:47 +0100

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,53 +1,27 @@
 #include <stdio.h>
-
 /**
+ *main - Entry Point
  *
- *  *main - Entry Point
+ * Return: Always 0 (success)
  *
- *   *
- *
- *    * Return: Always 0 (success)
- *
- *     *
- *
- *      */
-
+ */
 int main(void)
-
 {
+	char Capital_letter = 'A';
+	char Small_letter = 'a';
 
-		char Capital_letter = 'A';
+	while (Small_letter <= 'z')
 
-			char Small_letter = 'a';
+	{
+		putchar(Small_letter);
+		Small_letter++;
+	}
+	while (Capital_letter <= 'Z')
+	{
+		putchar(Capital_letter);
+		Capital_letter++;
+	}
 
-
-
-				while (Small_letter <= 'z')
-
-
-
-						{
-
-									putchar(Small_letter);
-
-											Small_letter++;
-
-												}
-
-					while (Capital_letter <= 'Z')
-
-							{
-
-										putchar(Capital_letter);
-
-												Capital_letter++;
-
-													}
-
-
-
-							putchar('\n');
-
-								return (0);
-
+		putchar('\n');
+	return (0);
 }
