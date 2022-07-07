5116ac518df9523a0e8035f95592a44d34dc81ae
chimi boris <57298424+chimiboris@users.noreply.github.com>
2022-07-07 17:31:27 +0100

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,35 +1,18 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Return: Always 0 (success)
  *
- *   *
- *
- *    * Return: Always 0 (success)
- *
- *     *
- *
- *      */
-
+ */
 int main(void)
-
 {
+	int base_10;
 
-		int base_10;
-
-
-
-			for (base_10 = 0; base_10 < 10; base_10++)
-
-					{
-
-								printf("%i", base_10);
-
-									}
-
-				printf("\n");
-
-					return (0);
-
+	for (base_10 = 0; base_10 < 10; base_10++)
+	{
+		printf("%i", base_10);
+	}
+	printf("\n");
+	return (0);
 }
