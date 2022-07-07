ffe867c3e3ec40984be94142253d303f660d7706
chimi boris <57298424+chimiboris@users.noreply.github.com>
2022-07-07 16:13:50 +0100

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,14 +1,13 @@
 nclude "main.h"
 
 /**
+ * print_sign - return 0 letter not lowercase, 1 letter lowercase
  *
- * * print_sign - return 0 letter not lowercase, 1 letter lowercase
+ *  *@n: the int to print
  *
- * *  *@n: the int to print
+ * Return: Always 0.
  *
- * * Return: Always 0.
- *
- * */
+ */
 
 int print_sign(int n)
 
