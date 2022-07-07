8872b78be6778f511409ecf49c50d87fa759e194
chimi boris <57298424+chimiboris@users.noreply.github.com>
2022-07-07 02:01:43 +0100

    Update 101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,31 +1,11 @@
-#include <stdio.h>
-
-#include <unistd.h>
-
-
-
-
-
+#include<unistd.h>
 /**
- *
- * * main - prints exactly "and that piece of art is useful"
- *
- * * - Dora Korpar, 2015-10-19",
- *
- * * followed by a new line, to the standard error.
- *
- * * Return: Always 0 (Success)
- *
- * */
-
-
-
+ *  * main - prints the standrad error sandwiched around the  standard outpu/t
+ *   *
+ *    * Return: returns 1
+ **/
 int main(void)
-
 {
-
 	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-
 	return (1);
-
 }
