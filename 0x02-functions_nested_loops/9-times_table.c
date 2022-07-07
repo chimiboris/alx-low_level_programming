c2597c0abc9401a6438a4a7274871657f009ceb4
chimi boris <57298424+chimiboris@users.noreply.github.com>
2022-07-07 16:16:21 +0100

    Update 9-times_table.c

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -1,12 +1,10 @@
 #include "main.h"
 
 /**
+ * times_table -prints tables
+ * Return: Always 0.
  *
- * * times_table -prints tables
- *
- * * Return: Always 0.
- *
- * */
+ */
 
 void times_table(void)
 
