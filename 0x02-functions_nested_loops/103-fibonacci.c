3fa1a7823e5b76d4eae6d529f1b1486685707035
chimi boris <57298424+chimiboris@users.noreply.github.com>
2022-07-07 16:21:32 +0100

    Update 103-fibonacci.c

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -3,16 +3,11 @@
 
 
 /**
- *
- *  * main - main block
- *
- *   * Description: computes and prints even  number < 4,000,000
- *
- *    * 5 below 1024 (excluded), followed by a new line
- *
- *     * Return: 0
- *
- *      */
+ * main - main block
+ * Description: computes and prints even  number < 4,000,000
+ * 5 below 1024 (excluded), followed by a new line
+ * Return: 0
+ */
 
 
 
