291be8a07901a2324b260c750149a0f28511068a
chimi boris <57298424+chimiboris@users.noreply.github.com>
2022-07-07 17:32:20 +0100

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,37 +1,19 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Return: Always 0 (Success)
  *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     *
- *
- *      */
-
+ */
 int main(void)
-
 {
+	int base_10;
 
-		int base_10;
+	for (base_10 = 48; base_10 < 58; base_10++)
 
-
-
-			for (base_10 = 48; base_10 < 58; base_10++)
-
-
-
-					{
-
-								putchar(base_10);
-
-									}
-
-				putchar('\n');
-
-					return (0);
-
+	{
+		putchar(base_10);
+	}
+	putchar('\n');
+	return (0);
 }
