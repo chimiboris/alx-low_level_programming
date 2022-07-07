18d2deba97fab2d6df6dbc7492df1124b902bb73
chimi boris <57298424+chimiboris@users.noreply.github.com>
2022-07-07 17:33:46 +0100

    Update 8-print_base16.c

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,47 +1,24 @@
 #include <stdio.h>
-
 /**
+ * main - Entry point
  *
- *  * main - Entry point
+ * Return: Always 0 (success)
  *
- *   *
- *
- *    * Return: Always 0 (success)
- *
- *     *
- *
- *      */
-
+ */
 int main(void)
-
 {
+	int hex;
+	char decimal = 'a';
 
-		int hex;
-
-			char decimal = 'a';
-
-
-
-				for (hex = 48; hex < 58; hex++)
-
-						{
-
-									putchar(hex);
-
-										}
-
-					while (decimal < 'g')
-
-							{
-
-										putchar(decimal);
-
-												decimal++;
-
-													}
-
-							putchar('\n');
-
-								return (0);
-
+	for (hex = 48; hex < 58; hex++)
+	{
+		putchar(hex);
+	}
+	while (decimal < 'g')
+	{
+		putchar(decimal);
+		decimal++;
+	}
+		putchar('\n');
+	return (0);
 }
