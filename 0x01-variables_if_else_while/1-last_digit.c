2f591473a64f9f54ac9f9c58f2effa361a7223af
chimi boris <57298424+chimiboris@users.noreply.github.com>
2022-07-07 13:23:56 +0100

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,11 +2,11 @@
 #include <time.h>
 #include <stdio.h>
 /**
- * main- Entry Point
- *
- * Return: Always 0 (success)
- *
- */
+* main- Entry Point
+*
+* Return: Always 0 (success)
+*
+*/
 int main(void)
 {
 	int n;
