4cbf9c7c4eb0e6f565f42847afc30bd2020221a7
chimiboris <chimiboris7@gmail.com>
2022-07-20 13:11:50 -0700

    She locked away

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,9 +1,9 @@
 #include "main.h"
 
 /**
- * _puts_recurssion -> this is a function to print string recursivly
- * @s: string parameter
- */
+* _puts_recurssion -> this is a function to print string recursivly
+* @s: string parameter
+*/
 void _puts_recursion(char *s)
 {
 	if (*s == '\0')
