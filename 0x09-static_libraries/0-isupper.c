563b43ad18829c9c632ee6b80c28779a09fc6066
chimi boris <57298424+chimiboris@users.noreply.github.com>
2022-07-25 19:37:35 +0100

    Update 0-isupper.c

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
-* _isupper - checks is a character is uppercase
-* @c: carrier integer variable
-* Return: 1 if true, 0 if false
-*/
+ * _isupper - checks is a character is uppercase
+ * @c: carrier integer variable
+ * Return: 1 if true, 0 if false
+ */
 
 int _isupper(int c)
 {
@@ -13,4 +13,5 @@ if (c >= 65 && c <= 90)
 return (1);
 }
 return (0);
+
 }
