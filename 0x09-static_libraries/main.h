6f3ef73fe901411a4026d565fa196f84e1d3f455
chimi boris <57298424+chimiboris@users.noreply.github.com>
2022-07-25 19:52:11 +0100

    Update main.h

--- 0x09-static_libraries/main.h
+++ 0x09-static_libraries/main.h
@@ -1,5 +1,9 @@
-#ifndef MAIN MAIN_H
+#ifndef MAIN_H
+
 #define MAIN_H
+
+
+
 int _putchar(char c);
 
 int _islower(int c);
@@ -39,5 +43,7 @@ unsigned int _strspn(char *s, char *accept);
 char *_strpbrk(char *s, char *accept);
 
 char *_strstr(char *haystack, char *needle);
+
+
 
 #endif
